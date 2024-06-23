0cbc8b9aee341facd6126af51a1f5a014c96a7ff
Anuj Jha <anujjha2106@gmail.com>
2024-06-23 12:27:51 +0530

    BInarySearch

---
+++ BinarySearch/A01BinarySearch.java
@@ -0,0 +1,24 @@
+public class A01BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 5, 7, 9, 10 };
+        int target = 4;
+        System.out.println(binarySearch(arr, target));
+    }
+
+    static boolean binarySearch(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            // int mid = (start + end) / 2;  // exceed the int range if start and end is larger value.
+            int mid = start + ((end-start)/2); 
+            if (target > arr[mid]) {
+                start = mid+1;
+            } else if (target<arr[mid]) {
+                end = mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
