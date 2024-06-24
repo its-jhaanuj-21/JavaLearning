1f240f67bee61a8e2adaafa8a20ad556680357fb
Anuj Jha <anujjha2106@gmail.com>
2024-06-24 23:36:05 +0530

    BubbleSort

---
+++ Sorting/A01BubbleSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class A01BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,9,11,2,6,4};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=1; j<arr.length-i; j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
